705d102ca81b56fa3200bb1ef06d0b56806ba914
Cary M. Robbins <carymrobbins@gmail.com>
2013-06-09 20:32:58 -0500

    Completed chapter 7, pending exercise 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleRails::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
 
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Cary Robbins"
+    email "cary@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
