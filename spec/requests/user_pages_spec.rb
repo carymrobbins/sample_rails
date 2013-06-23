4639c2eb1ff1ad0b73b40a12c4548a185b1e5641
Cary M. Robbins <carymrobbins@gmail.com>
2013-06-22 22:21:03 -0500

    Completed Chapter 7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe 'UserPages' do
 
   subject { page }
 
-  describe "profile page" do
+  describe 'profile page' do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
@@ -12,43 +12,49 @@ describe "UserPages" do
     it { should have_selector('title', text: user.name) }
   end
 
-  describe "signup page" do
+  describe 'signup page' do
     before { visit signup_path }
 
     it { should have_selector('h1', text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
   end
 
-  describe "signup" do
+  describe 'signup' do
 
     before { visit signup_path }
 
-    let(:submit) { "Create my account" }
+    let(:submit) { 'Create my account' }
 
-    describe "with invalid information" do
-      it "should not create a user" do
+    describe 'with invalid information' do
+      it 'should not create a user' do
         expect { click_button submit }.not_to change(User, :count)
       end
 
-      describe "after submission" do
+      describe 'after submission' do
         before { click_button submit }
 
         it { should have_selector('title', text: 'Sign up') }
-        it { should have_content('error') }
+        it { should have_content("Name can't be blank") }
+        it { should have_content("Email can't be blank") }
+        it { should have_content("Password can't be blank") }
+        it { should have_content("Password confirmation can't be blank") }
+        it { should_not have_content("Password digest can't be blank") }
       end
     end
 
-    describe "with valid information" do
+    describe 'with valid information' do
       before do
-        fill_in "Name", with: "Example User"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in 'Name', with: 'Example User'
+        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Password', with: 'foobar'
+        fill_in 'Confirmation', with: 'foobar'
       end
 
-      it "should create a user" do
+      it 'should create a user' do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      #describe 'after sav'
     end
   end
 end
