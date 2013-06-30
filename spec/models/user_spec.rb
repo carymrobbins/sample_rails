6376e8d72c3d6d27463c46bc907f3de6907a37cc
Cary M. Robbins <carymrobbins@gmail.com>
2013-06-30 00:22:17 -0500

    Next - 8.2.3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,8 +14,8 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: 'Example User', email: 'user@example.com',
+                     password: 'foobar', password_confirmation: 'foobar')
   end
 
   subject { @user }
@@ -25,27 +25,28 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
-  describe "when name is not present" do
-    before { @user.name = " " }
+  describe 'when name is not present' do
+    before { @user.name = ' ' }
     it { should_not be_valid }
   end
 
-  describe "when email is not present" do
-    before { @user.email = " " }
+  describe 'when email is not present' do
+    before { @user.email = ' ' }
     it { should_not be_valid }
   end
 
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
+  describe 'when name is too long' do
+    before { @user.name = 'a' * 51 }
     it { should_not be_valid }
   end
 
-  describe "when email format is invalid" do
-    it "should be invalid" do
+  describe 'when email format is invalid' do
+    it 'should be invalid' do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+bax.com]
       addresses.each do |invalid_address|
@@ -55,8 +56,8 @@ describe User do
     end
   end
 
-  describe "when email format is valid" do
-    it "should be valid" do
+  describe 'when email format is valid' do
+    it 'should be valid' do
       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
       addresses.each do |valid_address|
         @user.email = valid_address
@@ -65,7 +66,7 @@ describe User do
     end
   end
 
-  describe "when email address is already taken" do
+  describe 'when email address is already taken' do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
@@ -75,41 +76,41 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "email address with mixed case" do
-    let(:mixed_case_email) { "Foo@ExAMple.COm" }
+  describe 'email address with mixed case' do
+    let(:mixed_case_email) { 'Foo@ExAMple.COm' }
 
-    it "should be saved as all lower case" do
+    it 'should be saved as all lower case' do
       @user.email = mixed_case_email
       @user.save
       @user.reload.email.should == mixed_case_email.downcase
     end
   end
 
-  describe "when password is not present" do
-    before { @user.password = @user.password_confirmation = " " }
+  describe 'when password is not present' do
+    before { @user.password = @user.password_confirmation = ' ' }
     it { should_not be_valid}
   end
 
   describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatch" }
+    before { @user.password_confirmation = 'mismatch' }
     it { should_not be_valid }
   end
 
-  describe "when password confirmation is nil" do
+  describe 'when password confirmation is nil' do
     before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
 
-  describe "return value of authenticate method" do
+  describe 'return value of authenticate method' do
     before { @user.save }
     let(:found_user) { User.find_by_email(@user.email) }
 
-    describe "with valid password" do
+    describe 'with valid password' do
       it { should == found_user.authenticate(@user.password) }
     end
 
-    describe "with invalid password" do
-      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    describe 'with invalid password' do
+      let(:user_for_invalid_password) { found_user.authenticate('invalid') }
 
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
@@ -119,5 +120,10 @@ describe User do
       before { @user.password = @user.password_confirmation = 'a' * 5 }
       it { should be_invalid }
     end
+  end
+
+  describe 'remember token' do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -6,8 +6,8 @@ describe 'Authentication' do
   describe 'signin page' do
     before { visit signin_path }
 
-    it { should have_selector('h1', 'Sign in') }
-    it { should have_selector('title', 'Sign in') }
+    it { should have_selector('h1', text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
   end
 
   describe 'signin' do
@@ -16,8 +16,13 @@ describe 'Authentication' do
     describe 'with invalid information' do
       before { click_button 'Sign in' }
 
-      it { should have_selector('title', 'Sign in') }
+      it { should have_selector('title', text: 'Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe 'after visiting another page' do
+        before { click_link 'Home' }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
     end
 
     describe 'with valid information' do
@@ -28,7 +33,7 @@ describe 'Authentication' do
         click_button 'Sign in'
       end
 
-      it { should have_selector(:title, text: user.name) }
+      it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
